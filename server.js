56b4849f722f6e0b5bdf7c1df2f61dcd5e742553
cchica121 <segiayeni@gmail.com>
2017-08-08 13:59:49 -0400

    heroku added in server

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var app = express();
 var bodyParser =require('body-parser');
 var routes=require('./route');
 
+
 app.use(bodyParser.json({extended:true}));
 app.use('./', routes);
 app.use(express.static(__dirname + '/public'));
 
-app.listen(5000, function(){
+var port = process.envPORT || 5000;
+app.listen(port, function(){
   console.log('server is running :]');
 });
