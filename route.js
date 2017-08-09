4117d5d030af214e2d7e5c55863a8f450ef3450a
cchica121 <segiayeni@gmail.com>
2017-08-09 15:32:50 -0400

    routing

--- route.js
+++ route.js
@@ -3,5 +3,15 @@ var route = express.Router();
 var pg =require('pg');
 var pool = require('./pg-connection-pool');
 
+route.get('/home', function(req, res, next) {
+  pool.query('SELECT * FROM post_content ORDER BY up_votes DESC limit 15').then(function(result) {
+    res.json(result.rows);
+  });
+});
+  route.get('/corktown', function(req, res, next) {
+    pool.query('SELECT * FROM post_content WHERE zip="48216"').then(function(result) {
+      res.json(result.rows);
+    });
+  });
 
 module.exports = route;
