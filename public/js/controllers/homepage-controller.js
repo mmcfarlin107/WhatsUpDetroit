68f55929e561b6abdb19caee08abd9258449eada
cchica121 <segiayeni@gmail.com>
2017-08-09 11:03:38 -0400

    added the zip code stuff

--- public/js/controllers/homepage-controller.js
+++ public/js/controllers/homepage-controller.js
@@ -1,5 +1,14 @@
 var app = angular.module('detroitMod');
 
-app.controller('homeCtrl', function($scope){
+app.controller('homeCtrl', function($scope, detroitFactory){
+	$scope.zip = 'getting info..'
+	detroitFactory.getLocation();
+
+	setTimeout(function () {
+        $scope.$apply(function () {
+            $scope.zip = detroitFactory.getZip() ;
+        });
+    }, 5000);
+
 
 });
