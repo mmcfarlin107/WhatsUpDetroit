91cad5c6153c1e7dd807d8025538a87a53d006f0
mmcfarlin107 <mcfarlin107@gmail.com>
2017-08-09 13:31:15 -0400

    SQL queries started

--- route.js
+++ route.js
@@ -3,5 +3,10 @@ var route = express.Router();
 var pg =require('pg');
 var pool = require('./pg-connection-pool');
 
+route.get('/home', function(req, res, next) {
+  pool.query('select * from postContent order by upVotes limit 15').then(function(result) {
+    res.json(result.rows);
+  });
+});
 
 module.exports = route;
