a0b3f5cc99d406f2561d16b13a9eb439cd4de2e1
mmcfarlin107 <mcfarlin107@gmail.com>
2017-08-09 11:07:47 -0400

    commiting so I can merge

--- public/js/app/config.js
+++ public/js/app/config.js
@@ -1,1 +1,37 @@
 var app = angular.module('detroitMod');
+
+app.config(function($routeProvider){
+$routeProvider
+    .when('/home', {
+      controller: 'partial-controller',
+      templateUrl: 'home.html'
+    })
+    .when('/downtown', {
+      controller: 'partial-controller',
+      templateUrl: 'downtown.html'
+    })
+    .when('/corktown', {
+      controller: 'partial-controller',
+      templateUrl: 'corktown.html'
+    })
+    .when('/woodbridge', {
+      controller: 'partial-controller',
+      templateUrl: 'woodbridge.html'
+    })
+    .when('/new-center', {
+      controller: 'partial-controller',
+      templateUrl: 'new-center.html'
+    })
+    .when('/east-central', {
+      controller: 'partial-controller',
+      templateUrl: 'east-central.html'
+    })
+    .when('/southwest', {
+      controller: 'partial-controller',
+      templateUrl: 'southwest.html'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    })
+// clsoing out config
+});
