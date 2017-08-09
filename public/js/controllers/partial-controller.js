5e3f4329a6126cfe95f99b6bff5c01e9f0c90420
mterrian <matthew.terrian@gmail.com>
2017-08-09 15:33:15 -0400

    initial lockout functionality

--- public/js/controllers/partial-controller.js
+++ public/js/controllers/partial-controller.js
@@ -3,36 +3,71 @@ var app = angular.module('detroitMod');
 app.controller('corktown', function($scope, $rootScope){
 	$scope.areaName = "Corktown"
 	$scope.thisZip = "48216"
+	if($scope.thisZip === $rootScope.zip) {
+		$scope.hide = false
+	} else {
+		$scope.hide = true
+	}
 });
 
 app.controller('downtown', function($scope, $rootScope){
 	$scope.areaName = "Downtown"
 	$scope.thisZip = "48226"
+	if($scope.thisZip === $rootScope.zip) {
+		$scope.hide = false
+	} else {
+		$scope.hide = true
+	}
 });
 
 app.controller('midtown', function($scope, $rootScope){
 	$scope.areaName = "Midtown"
 	$scope.thisZip = "48201"
+	if($scope.thisZip === $rootScope.zip) {
+		$scope.hide = false
+	} else {
+		$scope.hide = true
+	}
 });
 
 app.controller('woodbridge', function($scope, $rootScope){
 	$scope.areaName = "Woodbridge"
 	$scope.thisZip = "48208"
+	if($scope.thisZip === $rootScope.zip) {
+		$scope.hide = false
+	} else {
+		$scope.hide = true
+	}
 });
 
 app.controller('newCenter', function($scope, $rootScope){
 	$scope.areaName = "New Center"
 	$scope.thisZip = "48202"
+	if($scope.thisZip === $rootScope.zip) {
+		$scope.hide = false
+	} else {
+		$scope.hide = true
+	}
 });
 
 app.controller('eastCentral', function($scope, $rootScope){
 	$scope.areaName = "East Central"
 	$scope.thisZip = "48207"
+	if($scope.thisZip === $rootScope.zip) {
+		$scope.hide = false
+	} else {
+		$scope.hide = true
+	}
 });
 
 app.controller('southwest', function($scope, $rootScope){
 	$scope.areaName = "Southwest"
 	$scope.thisZip = "48209"
+	if($scope.thisZip === $rootScope.zip) {
+		$scope.hide = false
+	} else {
+		$scope.hide = true
+	}
 });
 
 /*
