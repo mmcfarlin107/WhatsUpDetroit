bf73a6ad1af9944055f53874ddc41d7a39ec62a2
mmcfarlin107 <mcfarlin107@gmail.com>
2017-08-09 13:34:02 -0400

    Adding SQL queries still

--- route.js
+++ route.js
@@ -4,9 +4,15 @@ var pg =require('pg');
 var pool = require('./pg-connection-pool');
 
 route.get('/home', function(req, res, next) {
-  pool.query('select * from postContent order by upVotes limit 15').then(function(result) {
+  pool.query('SELECT * FROM postContent ORDER BY upVotes DESC limit 15').then(function(result) {
     res.json(result.rows);
   });
+
+  route.get('/corktown', function(req, res, next) {
+    pool.query('SELECT * FROM postContent ORDER BY upVotes DESC limit 15').then(function(result) {
+      res.json(result.rows);
+    });
+
 });
 
 module.exports = route;
